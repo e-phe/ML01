51ad1383c6d83855c817f6b37939e4ff317fc7b1
ephe <ephe@student42.fr>
2022-03-18 16:50:43 +0100

    fix-ex6-minmax

--- ex06/minmax.py
+++ ex06/minmax.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3.8
 # -*- coding: utf-8 -*-
 
-from locale import normalize
 import numpy as np
+
 
 def minmax(x):
     """Computes the normalized version of a non-empty numpy.array using the min-max standardization.
@@ -20,11 +20,12 @@ def minmax(x):
         return np.ravel(normalized)
     return
 
+
 if __name__ == "__main__":
-    X = np.array([[0],[ 15],[ -9],[ 7],[ 12],[ 3],[ -21]])
+    X = np.array([[0], [15], [-9], [7], [12], [3], [-21]])
     print(minmax(X))
     # Output: array([0.58333333, 1., 0.33333333, 0.77777778, 0.91666667, 0.66666667, 0.])
 
-    Y = np.array([[2],[ 14],[ -13],[ 5],[ 12],[ 4],[ -19]])
+    Y = np.array([[2], [14], [-13], [5], [12], [4], [-19]])
     print(minmax(Y))
     # Output: array([0.63636364, 1., 0.18181818, 0.72727273, 0.93939394, 0.6969697 , 0.])
